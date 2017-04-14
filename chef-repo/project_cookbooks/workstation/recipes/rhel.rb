6e11b58e327edd41de24d847067f2de3eb7f03e0
Sam Habiel <sam.habiel@gmail.com>
2017-04-14 13:12:11 -0400

    Fixes for RHEL/Centos workstation script

--- chef-repo/project_cookbooks/workstation/recipes/rhel.rb
+++ chef-repo/project_cookbooks/workstation/recipes/rhel.rb
@@ -5,15 +5,18 @@
 
 include_recipe "nokogiri_wrapper"
 
-include_recipe "chef-dk_wrapper"
+# Already installed!
+#include_recipe "chef-dk_wrapper"
 
 include_recipe "java_wrapper"
 
 include_recipe "gradle_wrapper"
 
-include_recipe "phantomjs_wrapper"
+# Done manually--Google moved URL
+#include_recipe "phantomjs_wrapper"
 
-include_recipe "workstation::git"
+# Already installed
+#include_recipe "workstation::git"
 
 include_recipe "build-essential"
 
@@ -27,6 +30,13 @@ execute "correct ownership of gem home" do
 	command "chown -R #{node[:workstation][:user]} #{node[:workstation][:user_home]}/Projects/vistacore/.gems"
 end
 
-include_recipe 'oracle_wrapper::client'
+# Don't know what that is
+#include_recipe 'oracle_wrapper::client'
 
-include_recipe "workstation::slave_config"
+# We are not slaves!
+#include_recipe "workstation::slave_config"
+
+#include_recipe 'virtualbox_wrapper'
+#node.default[:vagrant_wrapper][:home] = "#{node[:workstation][:user_home]}/Projects/vistacore/.vagrant.d"
+#include_recipe 'vagrant_wrapper'
+#include_recipe 'vagrant_wrapper::windows_plugin'
