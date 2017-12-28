21749e44a7771c457a83dc91613118cee2524baa
Sam Habiel <Sam.Habiel@gmail.com>
2017-12-27 16:25:03 -0800

    Unredaction part 3, Mocks VM now works

--- chef-repo/machine_cookbooks/ehmp_provision/attributes/mocks.rb
+++ chef-repo/machine_cookbooks/ehmp_provision/attributes/mocks.rb
@@ -8,15 +8,15 @@ default[:ehmp_provision][:mocks][:copy_files] = {}
 #######################################################################################################################
 # mocks specific aws configuration options
 default[:ehmp_provision][:mocks][:aws][:instance_type] = "m3.medium"
-default[:ehmp_provision][:mocks][:aws][:subnet] = "DNS"
-default[:ehmp_provision][:mocks][:aws][:ssh_username] = "USER    "
-default[:ehmp_provision][:mocks][:aws][:ssh_keyname] = "SSH KEYNAME"
+default[:ehmp_provision][:mocks][:aws][:subnet] = "subnet-213b2256"
+default[:ehmp_provision][:mocks][:aws][:ssh_username] = "PW      "
+default[:ehmp_provision][:mocks][:aws][:ssh_keyname] = "redacted"
 default[:ehmp_provision][:mocks][:aws][:ssh_key_path] = "#{ENV['HOME']}/Projects/vistacore/.chef/keys/#{node[:ehmp_provision][:mocks][:aws][:ssh_keyname]}"
 #######################################################################################################################
 
 #######################################################################################################################
 # mocks specific vagrant configuration options
-default[:ehmp_provision][:mocks][:vagrant][:ip_address] = "IP        "
+default[:ehmp_provision][:mocks][:vagrant][:ip_address] = "172.16.4.205"
 default[:ehmp_provision][:mocks][:vagrant][:provider_config] = {
   :memory => 1024
 }
