257fe3e3bda674473d903f16caf4f192cc036688
Sam Habiel <sam.habiel@gmail.com>
2018-01-10 13:57:50 -0500

    node[:nexus_url] comes empty for workstation cookbook. So hardcode it for what we need

--- chef-repo/project_cookbooks/workstation/recipes/default.rb
+++ chef-repo/project_cookbooks/workstation/recipes/default.rb
@@ -5,7 +5,13 @@
 
 #Load Nexus_url from common cookbook
 include_recipe "common::load_nexus_url"
-node.default[:nexus_url] = node[:common][:nexus_url]
+node.normal[:nexus_url] = node[:common][:nexus_url]
+
+log 'nexus_url' do
+  message node[:nexus_url]
+  level :info
+end
+Chef::Log.info(node[:nexus_url])
 
 
 
--- chef-repo/wrapper_cookbooks/gradle_wrapper/attributes/default.rb
+++ chef-repo/wrapper_cookbooks/gradle_wrapper/attributes/default.rb
@@ -4,7 +4,7 @@
 #
 #
 
-default[:gradle][:url] = "#{node[:nexus_url]}/nexus/content/repositories/filerepo/third-party/program/gradle/gradle/2.4/gradle-2.4-bin.zip"
+default[:gradle][:url] = "http://nexus.osehra.org:8081/nexus/content/repositories/filerepo/third-party/program/gradle/gradle/2.4/gradle-2.4-bin.zip"
 default[:gradle][:version] = "2.4"
 default[:gradle][:checksum] = "c4eaecc621a81f567ded1aede4a5ddb281cc02a03a6a87c4f5502add8fc2f16f"
 default[:gradle][:home] = "/usr/local/gradle/gradle-#{node[:gradle][:version]}"
